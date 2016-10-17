f3bbfe2d9b375f09de9c52600044774b736afe20
juananrey <ja.reymendo@gmail.com>
2016-10-17 20:50:07 +0200

    Fixed booboo where shopping cart tried to read anything on the session, instead of the variables under prefix cart/

--- src/ShoppingCartBundle/Entity/ShoppingCart.php
+++ src/ShoppingCartBundle/Entity/ShoppingCart.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ShoppingCart implements ShoppingCartInterface
 {
-    const CART_NAMESPACE = 'cart';
+    const CART_NAMESPACE = 'cart/';
     private $session;
 
     public function __construct(SessionInterface $session)
@@ -16,36 +16,38 @@ class ShoppingCart implements ShoppingCartInterface
 
     public function has($name)
     {
-        $name = self::CART_NAMESPACE.'/'.$name;
+        $name = self::CART_NAMESPACE.$name;
         return $this->session->has($name);
     }
 
     public function get($name)
     {
-        $name = self::CART_NAMESPACE.'/'.$name;
+        $name = self::CART_NAMESPACE.$name;
         return $this->session->get($name);
     }
 
     public function set($name, $value)
     {
-        $name = self::CART_NAMESPACE.'/'.$name;
+        $name = self::CART_NAMESPACE.$name;
         $this->session->set($name, $value);
     }
 
     public function remove($name)
     {
-        $name = self::CART_NAMESPACE.'/'.$name;
+        $name = self::CART_NAMESPACE.$name;
         return $this->session->remove($name);
     }
 
     public function all()
     {
         $result = [];
-
         $allValues = $this->session->all();
+
         foreach ($allValues as $key => $value) {
-            $newKey = str_replace(self::CART_NAMESPACE.'/', '', $key);
-            $result[$newKey] = $value;
+            if (false !== strpos($key, self::CART_NAMESPACE)) {
+                $newKey = str_replace(self::CART_NAMESPACE, '', $key);
+                $result[$newKey] = $value;
+            }
         }
 
         return $result;
